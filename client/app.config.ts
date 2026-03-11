9a449cbe8cd6d1d134e1f2497bafbcc6e87ec57e
yazi8929 <1002887616@qq.com>
2026-03-11 14:44:04 +0800

    Update app.config.ts

--- client/app.config.ts
+++ client/app.config.ts
@@ -31,12 +31,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       "favicon": "./assets/images/favicon.png"
     },
     "plugins": [
-      process.env.EXPO_PUBLIC_BACKEND_BASE_URL ? [
-        "expo-router",
-        {
-          "origin": process.env.EXPO_PUBLIC_BACKEND_BASE_URL
-        }
-      ] : 'expo-router',
+      'expo-router',
       [
         "expo-splash-screen",
         {
