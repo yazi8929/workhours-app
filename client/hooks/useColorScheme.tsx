aa50b9d098e3823a9671e598a2b230d78ded6384
yazi8929 <1002887616@qq.com>
2026-03-14 20:05:48 +0800

    Update useColorScheme.tsx

--- client/hooks/useColorScheme.tsx
+++ client/hooks/useColorScheme.tsx
@@ -1,48 +1,19 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from 'react';
-import { ColorSchemeName, useColorScheme as useReactNativeColorScheme, Platform } from 'react-native';
+import { createContext, ReactNode, useContext } from 'react';
+import { useColorScheme as useReactNativeColorScheme } from 'react-native';
 
 const ColorSchemeContext = createContext<'light' | 'dark' | null | undefined>(null);
 
 const ColorSchemeProvider = function ({ children }: { children?: ReactNode }) {
-  const systemColorScheme = useReactNativeColorScheme();
-  const [colorScheme, setColorScheme] = useState(systemColorScheme);
-
-  useEffect(() => {
-    setColorScheme(systemColorScheme);
-  }, [systemColorScheme]);
-
-  useEffect(() => {
-    function handleMessage(e: MessageEvent<{ event: string; colorScheme: ColorSchemeName; } | undefined>) {
-      if (e.data?.event === 'coze.workbench.colorScheme') {
-        const cs = e.data.colorScheme;
-        if (typeof cs === 'string' && typeof setColorScheme === 'function') {
-          setColorScheme(cs);
-        }
-      }
-    }
-
-    if (Platform.OS === 'web') {
-      window.addEventListener('message', handleMessage, false);
-    }
-
-    return () => {
-      if (Platform.OS === 'web') {
-        window.removeEventListener('message', handleMessage, false);
-      }
-    }
-  }, [setColorScheme]);
-
-  return <ColorSchemeContext.Provider value={colorScheme}>
-    {children}
-  </ColorSchemeContext.Provider>
+  const colorScheme = useReactNativeColorScheme();
+  return (
+    <ColorSchemeContext.Provider value={colorScheme}>
+      {children}
+    </ColorSchemeContext.Provider>
+  );
 };
 
 function useColorScheme() {
-  const colorScheme = useContext(ColorSchemeContext);
-  return colorScheme;
+  return useContext(ColorSchemeContext);
 }
 
-export {
-  ColorSchemeProvider,
-  useColorScheme,
-}
+export { ColorSchemeProvider, useColorScheme }
