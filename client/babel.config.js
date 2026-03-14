6b54cbd71b7a956d0ecd0759f2eea80f9c4086de
yazi8929 <1002887616@qq.com>
2026-03-14 20:04:52 +0800

    Update babel.config.js

--- client/babel.config.js
+++ client/babel.config.js
@@ -3,7 +3,6 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // react-native-reanimated 必须放在 plugins 列表的最后
       'react-native-reanimated/plugin',
     ],
   };
